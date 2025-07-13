c6d359333187413c73e893db74005fd70f034a9e
pakowanko <wojtek@pakowanko.com>
2025-07-13 12:31:20 +0200

    CHORE: Porządki i finalizacja kodu frontendu

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,3 @@
-const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://foodtruck-backend-prod-XXXX.run.app' // UWAGA: Wklej tutaj finalny adres URL z Cloud Run
-  : 'http://localhost:3000';
-
+// src/apiConfig.js
+const API_URL = '';
 export default API_URL;
