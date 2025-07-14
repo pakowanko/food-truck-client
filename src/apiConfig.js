c085cd5187be058a5fc8542cc9342133a7432bbe
pakowanko <wojtek@pakowanko.com>
2025-07-14 20:10:33 +0200

    FIX: Finalne poprawki ścieżek importu we frontendzie

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,15 +1,13 @@
 // src/apiConfig.js
 import axios from 'axios';
 
-// Adres URL dla zapytań przez Firebase Hosting (rewrites)
-const API_BASE_URL = ''; 
-
 // Pełny adres URL do bezpośrednich połączeń (np. Socket.IO)
 const DIRECT_URL = 'https://food-truck-backend-1035693089076.europe-west1.run.app';
 
 // Tworzymy i eksportujemy skonfigurowaną instancję axios
+// ZMIANA: Dodajemy `baseURL: '/api'`, aby wszystkie zapytania były poprawne
 export const api = axios.create({
-  baseURL: API_BASE_URL
+  baseURL: '/api' 
 });
 
 // Eksportujemy pozostałe stałe
