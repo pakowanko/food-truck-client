f6dbea4cd3a97ddc9305d9bf7f12414df9ff6e63
pakowanko <wojtek@pakowanko.com>
2025-07-15 17:14:10 +0200

    FIX: Finalne poprawki ścieżek importu we frontendzie

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,15 +1,12 @@
 // src/apiConfig.js
 import axios from 'axios';
 
-// Pełny adres URL do bezpośrednich połączeń (np. Socket.IO)
+// TO JEST TO MIEJSCE:
 const DIRECT_URL = 'https://food-truck-backend-1035693089076.europe-west1.run.app';
 
-// Tworzymy i eksportujemy skonfigurowaną instancję axios
-// ZMIANA: Dodajemy `baseURL: '/api'`, aby wszystkie zapytania były poprawne
 export const api = axios.create({
-  baseURL: '/api' 
+  baseURL: '/api'
 });
 
-// Eksportujemy pozostałe stałe
 export const SOCKET_URL = DIRECT_URL;
 export const DIRECT_BACKEND_URL = DIRECT_URL;
